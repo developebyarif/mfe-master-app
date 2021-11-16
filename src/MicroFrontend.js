ae83dcf6a547b2c15b4480e707a015a5b58c90dc
Arif <developebyarif@gmail.com>
2021-11-16 13:37:04 +0530

    fix: local & production asset path changes

--- src/MicroFrontend.js
+++ src/MicroFrontend.js
@@ -10,15 +10,37 @@ class MicroFrontend extends React.Component {
       return;
     }
 
+    console.log('\n ENV :', process.env.NODE_ENV);
+
     fetch(`${host}/asset-manifest.json`)
       .then(res => res.json())
       .then(manifest => {
-        const script = document.createElement('script');
-        script.id = scriptId;
-        script.crossOrigin = '';
-        script.src = `${host}${manifest['files']['main.js']}`;
-        script.onload = this.renderMicroFrontend;
-        document.head.appendChild(script);
+
+
+        if (process.env.NODE_ENV == "development") {
+          const script = document.createElement('script');
+          
+          script.id = scriptId;
+          script.crossOrigin = '';
+          script.src = `${host}${manifest['files']['main.js']}`;
+          
+          script.onload = this.renderMicroFrontend;
+          document.head.appendChild(script);
+          
+        } else {
+          const script = document.createElement('script');
+          const link = document.createElement('link');
+          script.id = scriptId;
+          script.crossOrigin = '';
+          // script.src = `${host}${manifest['files']['main.js']}`;
+          script.src = `${manifest['files']['main.js']}`;
+          link.href = `${manifest['files']['main.css']}`;
+          script.onload = this.renderMicroFrontend;
+          document.head.appendChild(script);
+          document.head.appendChild(link);
+        }
+
+
       });
   }
 
